48db968a062baffb5b2a5f7a30174609a9468a30
iSteve-O <iSteve-O@users.noreply.github.com>
2023-08-20 03:44:29 -0400

    Update index.js
    add extensive logging for testing

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class MultiSwitcheroo {
   constructor(log, config) {
     this.log = log;
     this.config = config; // Store the config object
+    this.log.info('MultiSwitcheroo plugin initialized');
     this.name = config.name;
     this.onUrl = config.onUrl;
     this.offUrl = config.offUrl;
@@ -25,8 +26,10 @@ class MultiSwitcheroo {
     this.serialNumber = config.serialNumber || this.name;
     this.firmwareRevision = config.firmwareRevision || pkgVersion;
     this.switches = [];
+    this.log.info('MultiSwitcheroo plugin initialization completed');
 
     for (const switchConfig of config.switches) {
+      this.log.info(`Creating switch: ${switchConfig.name}`); //log each switch creation
       const switchName = switchConfig.name;
       const switchService = new Service.Switch(switchName, switchName);
       switchService
@@ -48,22 +51,25 @@ class MultiSwitcheroo {
         statusemitter.on('longpoll', (data) => {
           const isOn = !!data.match(switchConfig.statusPattern);
           switchService.getCharacteristic(Characteristic.On).updateValue(isOn);
+          this.log.info(`Polling status for ${switchConfig.name}: ${isOn}`); //log polling status
         });
 
         statusemitter.on('error', (error) => {
-          this.log(`Polling error: ${error}`);
+          this.log.warn(`Polling error: ${error}`);
         });
       }
 
       this.switches.push(switchService);
+      this.log.info(`Switch created: ${switchConfig.name}`); //log each switch when done
     }
-
+    this.log.info('MultiSwitcheroo device creation completed'); //log creation of the device
     this.informationService = new Service.AccessoryInformation();
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serialNumber)
       .setCharacteristic(Characteristic.FirmwareRevision, this.firmwareRevision);
+    this.log.info(`${this.name} initialized...\nModel: ${this.model}\nManufacturer: ${this.manufacturer}\nSerial Number: ${this.serialNumber}\nFirmware Version: ${this.firmwareRevision}`); // Log the device details
   }
 
   setOn(on, callback, switchConfig) {
@@ -72,8 +78,10 @@ class MultiSwitcheroo {
       rejectUnauthorized: false
     }, (err, response, body) => {
       if (!err && response.statusCode === 200) {
+        this.log.info(`${switchConfig.name} toggled successfully`); // log the request
         callback(null);
       } else {
+        this.log.warn(`ERROR SETTING ${switchConfig.name}, CODE: ${response ? response.statusCode : 'N/A'}`); //log any errors
         callback(err || new Error(`Invalid response: ${response.statusCode}`));
       }
     });
@@ -87,8 +95,10 @@ class MultiSwitcheroo {
     }, (err, response, body) => {
       if (!err && response.statusCode === 200) {
         const isOn = !!body.match(switchConfig.statusPattern);
+        this.log.info(`Status Request: ${this.config.statusUrl}`); // log the request
         callback(null, isOn);
       } else {
+        this.log.warn(`REQUEST ERROR: ${this.config.statusUrl}, CODE: ${response ? response.statusCode : 'N/A'}`); //log any errors
         callback(err || new Error(`Invalid response: ${response.statusCode}`));
       }
     });
