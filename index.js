b774437fd3362595b081079c431d0c87b487498e
iSteve-O <iSteve-O@users.noreply.github.com>
2023-08-21 02:56:56 -0400

    Update index.js
    update log levels

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class MultiSwitcheroo {
         .on('get', (callback) => { this.getOn(callback, switchConfig); });
 
       if (this.config.statusUrl && switchConfig.statusPattern) {
-        this.log.info(`Emitter pattern: ${switchConfig.statusPattern}`);
+        this.log.debug(`Emitter pattern: ${switchConfig.statusPattern}`);
         const statusemitter = pollingtoevent((done) => {
           axios.get(this.config.statusUrl, { rejectUnauthorized: false })
             .then((response) => done(null, response.data))
@@ -43,7 +43,7 @@ class MultiSwitcheroo {
         }, { longpolling: true, interval: this.config.pollingInterval });
 
         statusemitter.on('longpoll', (data) => {
-          this.log.info(`Received status data:`, data);  // remove the slashes & parenthesis & semi to see all data in logs -Log the received data
+          this.log.debug(`Received status data:`, data);  // remove the slashes & parenthesis & semi to see all data in logs -Log the received data
           // const dataString = JSON.stringify(data); // Convert the JSON object to a string
           // this.log('String data:'); //, dataString); <remove the slashes & parenthesis & semi to see all data in logs -Log the converted data
           const isOn = !!JSON.stringify(data).match(switchConfig.statusPattern);
@@ -73,7 +73,7 @@ class MultiSwitcheroo {
     axios.get(on ? switchConfig.onUrl : switchConfig.offUrl, { rejectUnauthorized: false })
       .then((response) => {
         if (response.status === 200) {
-          this.log.info(`${switchConfig.name} toggled successfully ${response.status}`);
+          this.log.debug(`${switchConfig.name} toggled successfully ${response.status}`);
           callback(null);
         } else {
           this.log.warn(`ERROR SETTING ${switchConfig.name}, CODE: ${response.status}`);
@@ -87,18 +87,17 @@ class MultiSwitcheroo {
   }
 
   getOn(callback, switchConfig) {
-    this.log.info(`getOn URL Rec'd: ${this.config.statusUrl}`);
-    this.log.info(`getOn Pattern Rec'd: ${switchConfig.statusPattern}`);
+    this.log.debug(`getOn URL Rec'd: ${this.config.statusUrl}`);
+    this.log.debug(`getOn Pattern Rec'd: ${switchConfig.statusPattern}`);
     if (!this.config.statusUrl || !switchConfig.statusPattern) return callback(null, false);
-    this.log.warn(`getOn statusPattern: ${switchConfig.statusPattern}`); // Log the status pattern ${switchConfig.statusPattern}
+    this.log.debug(`getOn statusPattern: ${switchConfig.statusPattern}`); // Log the status pattern ${switchConfig.statusPattern}
     axios.get(this.config.statusUrl, { rejectUnauthorized: false })
       .then((response) => {
         if (response.status === 200) {
-          this.log.warn(`getOn Response Data:`, (response.data)); //log the response
+          this.log.debug(`getOn Response Data:`, (response.data)); //log the response
           const isOn = !!JSON.stringify(response.data).match(switchConfig.statusPattern); //remove the 2 lines above to reinstate
-          this.log.info(`getOn Status URL: ${this.config.statusUrl}`);
-          this.log.info(`getOn config Pattern: ${this.config.statusPattern}`);
-          this.log.warn(`getOn switchConfig Pattern: ${switchConfig.statusPattern}`);
+          this.log.debug(`getOn Status URL: ${this.config.statusUrl}`);
+          this.log.debug(`getOn switchConfig Pattern: ${switchConfig.statusPattern}`);
           callback(null, isOn);
         } else {
           this.log.warn(`getOn REQUEST ERROR: ${this.config.statusUrl}, CODE: ${response.status}`);
