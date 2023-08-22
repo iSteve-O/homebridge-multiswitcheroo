135b81d3306227d2f3cb1cc62e353af2dff18e0b
iSteve-O <iSteve-O@users.noreply.github.com>
2023-08-22 08:36:18 -0400

    Update index.js
    back to `axios`, but try to use headers to prevent response caching.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const http = require("homebridge-http-base").http;
+const axios = require('axios');
+const pollingtoevent = require('polling-to-event');
 const pkgVersion = require('./package.json').version;
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.headers = {
+  'Cache-Control': 'no-cache',
+  'Pragma': 'no-cache',
+  'Expires': '0',
+};
 
 let Service, Characteristic;
 
@@ -12,8 +20,7 @@ module.exports = (homebridge) => {
 class MultiSwitcheroo {
   constructor(log, config) {
     this.log = log;
-    this.config = config;
-
+    this.config = config; // Store the config object
     this.name = config.name;
     this.onUrl = config.onUrl;
     this.offUrl = config.offUrl;
@@ -25,13 +32,47 @@ class MultiSwitcheroo {
     this.serialNumber = config.serialNumber || this.name;
     this.firmwareRevision = config.firmwareRevision || pkgVersion;
     this.switches = [];
-    this.currentStatus = {}; // Store the current status of each switch
+
+    const statusemitter = pollingtoevent((done) => {
+      if (this.config.statusUrl) {
+        //this.log.debug(`Emitter calling URL`);
+        axiosInstance.get(this.config.statusUrl, { rejectUnauthorized: false })
+          .then((response) => done(null, response.data))
+          .catch((error) => {
+            this.log.error(`Error fetching status data: ${error.message}`);
+            done(error, null);
+          });
+      } else {
+        this.log.warn(`Error: No statusURL defined in config.json`);
+        done(null, null); // No status URL defined
+      }
+    }, { longpolling: true, interval: this.config.pollingInterval });
+
+    statusemitter.on('longpoll', (data) => {
+      //this.log.debug(`Parsing URL data`);
+      //this.log.debug(`Received status data:`, data);
+      const statusData = JSON.stringify(data);
+      for (const switchService of this.switches) {
+        const switchConfig = switchService.switchConfig;
+        if (switchConfig.statusPattern) {
+          const isOn = !!statusData.match(switchConfig.statusPattern);
+          switchService.getCharacteristic(Characteristic.On).updateValue(isOn);
+          //if (!isOn && switchService.getCharacteristic(Characteristic.On).value) {
+            //this.log.warn(`$(switchConfig.name) is off in the Home app, but should be on bsed on server respone.`);
+            //switchService.getCharacteristic(Characteristic.On).setValue(true);
+          //}
+        }
+      }
+    });
+
+    statusemitter.on('error', (error) => {
+      this.log.error(`Polling error: ${error}`);
+    });
 
     for (const switchConfig of config.switches) {
       const switchName = switchConfig.name;
       const switchService = new Service.Switch(switchName, switchName);
-      switchService.switchConfig = switchConfig;
-
+      switchService.switchConfig = switchConfig; // Attach switchConfig to the service
       switchService
         .getCharacteristic(Characteristic.On)
         .on('set', (on, callback) => { this.setOn(on, callback, switchConfig); })
@@ -41,64 +82,70 @@ class MultiSwitcheroo {
       this.log.info(`Switch created: ${switchConfig.name}`);
     }
 
-    if (config.pollingInterval) {
-      this.setupStatusPolling();
-    }
-  }
-
-  setupStatusPolling() {
-    setInterval(() => {
-      this.updateAllStatus();
-    }, this.config.pollingInterval);
-  }
-
-  updateAllStatus() {
-    http.httpRequest(this.config.statusUrl, (error, response, body) => {
-      if (error) {
-        this.log("updateAllStatus() failed: %s", error.message);
-      } else if (!http.isHttpSuccessCode(response.statusCode)) {
-        this.log("updateAllStatus() http request returned http error code: %s", response.statusCode);
-      } else {
-        const statusData = body;
-        for (const switchService of this.switches) {
-          const switchConfig = switchService.switchConfig;
-          const isOn = statusData.includes(switchConfig.statusPattern);
-          this.currentStatus[switchConfig.name] = isOn;
-          switchService.getCharacteristic(Characteristic.On).updateValue(isOn);
-        }
-      }
-    });
-  }
-
-  setOn(on, callback, switchConfig) {
-    const url = on ? switchConfig.onUrl : switchConfig.offUrl;
-    http.httpRequest(url, (error, response, body) => {
-      if (error) {
-        this.log("setOn() failed: %s", error.message);
-        callback(error);
-      } else if (!http.isHttpSuccessCode(response.statusCode)) {
-        this.log("setOn() http request returned http error code: %s", response.statusCode);
-        callback(new Error("Got html error code " + response.statusCode));
-      } else {
-        this.log("Switch set to %s", on ? "ON" : "OFF");
-        this.updateAllStatus(); // Update all statuses after setting
-        callback();
-      }
-    });
-  }
-
-  getOn(callback, switchConfig) {
-    const isOn = this.currentStatus[switchConfig.name] || false;
-    callback(null, isOn);
-  }
-
-  getServices() {
-    const informationService = new Service.AccessoryInformation()
+    this.informationService = new Service.AccessoryInformation();
+    this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serialNumber)
       .setCharacteristic(Characteristic.FirmwareRevision, this.firmwareRevision);
 
-    return [informationService, ...this.switches];
+    this.log.info(`${this.name} initialized...\n\t|Model: ${this.model}\n\t|Manufacturer: ${this.manufacturer}\n\t|Serial Number: ${this.serialNumber}\n\t|Firmware Version: ${this.firmwareRevision}`);
+  }
+
+  setOn(on, callback, switchConfig) {
+    //this.log.debug(`setOn calling URL`);
+    axiosInstance.get(on ? switchConfig.onUrl : switchConfig.offUrl, { rejectUnauthorized: false })
+      .then((response) => {
+        if (response.status === 200) {
+          //this.log.debug(`${switchConfig.name} toggled successfully ${response.status}`);
+          callback(null);
+        } else {
+          this.log.warn(`ERROR SETTING ${switchConfig.name}, CODE: ${response.status}`);
+          callback(new Error(`setOn Invalid response: ${response.status}`));
+        }
+      })
+      .catch((error) => {
+        this.log.error(`setOn ERROR SETTING ${switchConfig.name}: ${error}`);
+        callback(error);
+      });
+  }
+
+  getOn(callback, switchConfig) {
+    //this.log.debug(`getOn URL Rec'd: ${this.config.statusUrl}`);
+    //this.log.debug(`getOn Pattern Rec'd: ${switchConfig.statusPattern}`);
+
+    if (!this.config.statusUrl || !switchConfig.statusPattern) {
+      return callback(null, false);
+      this.log.warn(`Make sure statusUrl & statusPattern are defined properly in your config`);
+    }
+
+    //this.log.debug(`getOn statusPattern: ${switchConfig.statusPattern}`);
+    //this.log.debug(`getOn calling URL`);
+    axiosInstance.get(this.config.statusUrl, { rejectUnauthorized: false })
+      .then((response) => {
+        if (response.status === 200) {
+          //this.log.debug(`getOn Response Data:`, response.data);
+
+          const statusData = JSON.stringify(response.data); // Parse the response data
+          const isOn = !!statusData.match(switchConfig.statusPattern);
+
+          //this.log.debug(`getOn Status URL: ${this.config.statusUrl}`);
+          //this.log.debug(`getOn switchConfig Pattern: ${switchConfig.statusPattern}`);
+
+          callback(null, isOn);
+          
+        } else {
+          this.log.warn(`getOn REQUEST ERROR: ${this.config.statusUrl}, CODE: ${response.status}`);
+          callback(new Error(`getOn Invalid response: ${response.status}`));
+        }
+      })
+      .catch((error) => {
+        this.log.error(`getOn REQUEST ERROR: ${this.config.statusUrl}, CODE: ${error}`);
+        callback(error);
+      });
+  }
+
+  getServices() {
+    return [this.informationService, ...this.switches];
   }
 }
