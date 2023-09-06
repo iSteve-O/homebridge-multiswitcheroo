7a2732ecf7c7b9b6708d4addc07b162defd9d13f
iSteve-O <iSteve-O@users.noreply.github.com>
2023-09-06 01:45:53 -0400

    Fix For Undefined Switch Array (#2)
    * Update index.js
    
    add check for valid config
    
    * Update package.json
    
    update version & branch for verification fixes
    
    * Update package.json
    
    ready 3.0.2 for merge with main OffFix Branch
    
    * Update index.js
    
    fix syntax and removed unused code

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ class MultiSwitcheroo {
 
     const statusemitter = pollingtoevent((done) => {
       if (this.config.statusUrl) {
-        //this.log.debug(`Emitter calling URL`);
         axios.get(this.config.statusUrl, { rejectUnauthorized: false })
           .then((response) => done(null, response.data))
           .catch((error) => {
@@ -42,18 +41,12 @@ class MultiSwitcheroo {
     }, { longpolling: false, interval: this.config.pollingInterval });
 
     statusemitter.on('poll', (data) => {
-      //this.log.debug(`Parsing URL data`);
-      //this.log.debug(`Received status data:`, data);
       const statusData = JSON.stringify(data);
       for (const switchService of this.switches) {
         const switchConfig = switchService.switchConfig;
         if (switchConfig.statusPattern) {
           const isOn = !!statusData.match(switchConfig.statusPattern);
           switchService.getCharacteristic(Characteristic.On).updateValue(isOn);
-          //if (!isOn && switchService.getCharacteristic(Characteristic.On).value) {
-            //this.log.warn(`$(switchConfig.name) is off in the Home app, but should be on bsed on server respone.`);
-            //switchService.getCharacteristic(Characteristic.On).setValue(true);
-          //}
         }
       }
     });
@@ -62,17 +55,21 @@ class MultiSwitcheroo {
       this.log.error(`Polling error: ${error}`);
     });
 
-    for (const switchConfig of config.switches) {
-      const switchName = switchConfig.name;
-      const switchService = new Service.Switch(switchName, switchName);
-      switchService.switchConfig = switchConfig; // Attach switchConfig to the service
-      switchService
-        .getCharacteristic(Characteristic.On)
-        .on('set', (on, callback) => { this.setOn(on, callback, switchConfig); })
-        .on('get', (callback) => { this.getOn(callback, switchConfig); });
+    if (!Array.isArray(config.switches)) { //check for a valid array of switches before trying to create them
+      this.log.error('MultiSwitcheroo switches array is not properly defined. Please define switches in the config & restart, or disable the plugin.');
+    } else {
+      for (const switchConfig of config.switches) {
+        const switchName = switchConfig.name;
+        const switchService = new Service.Switch(switchName, switchName);
+        switchService.switchConfig = switchConfig; // Attach switchConfig to the service
+        switchService
+          .getCharacteristic(Characteristic.On)
+          .on('set', (on, callback) => { this.setOn(on, callback, switchConfig); })
+          .on('get', (callback) => { this.getOn(callback, switchConfig); });
 
-      this.switches.push(switchService);
-      this.log.info(`Switch created: ${switchConfig.name}`);
+        this.switches.push(switchService);
+        this.log.info(`Switch created: ${switchConfig.name}`);
+      }
     }
 
     this.informationService = new Service.AccessoryInformation();
@@ -86,11 +83,9 @@ class MultiSwitcheroo {
   }
 
   setOn(on, callback, switchConfig) {
-    //this.log.debug(`setOn calling URL`);
     axios.get(on ? switchConfig.onUrl : switchConfig.offUrl, { rejectUnauthorized: false })
       .then((response) => {
         if (response.status === 200) {
-          //this.log.debug(`${switchConfig.name} toggled successfully ${response.status}`);
           callback(null);
         } else {
           this.log.warn(`ERROR SETTING ${switchConfig.name}, CODE: ${response.status}`);
@@ -104,26 +99,20 @@ class MultiSwitcheroo {
   }
 
   getOn(callback, switchConfig) {
-    //this.log.debug(`getOn URL Rec'd: ${this.config.statusUrl}`);
-    //this.log.debug(`getOn Pattern Rec'd: ${switchConfig.statusPattern}`);
 
     if (!this.config.statusUrl || !switchConfig.statusPattern) {
       return callback(null, false);
       this.log.warn(`Make sure statusUrl & statusPattern are defined properly in your config`);
     }
 
-    //this.log.debug(`getOn statusPattern: ${switchConfig.statusPattern}`);
-    //this.log.debug(`getOn calling URL`);
+
     axios.get(this.config.statusUrl, { rejectUnauthorized: false })
       .then((response) => {
         if (response.status === 200) {
-          //this.log.debug(`getOn Response Data:`, response.data);
-
+          
           const statusData = JSON.stringify(response.data); // Parse the response data
           const isOn = !!statusData.match(switchConfig.statusPattern);
 
-          //this.log.debug(`getOn Status URL: ${this.config.statusUrl}`);
-          //this.log.debug(`getOn switchConfig Pattern: ${switchConfig.statusPattern}`);
 
           callback(null, isOn);
           
